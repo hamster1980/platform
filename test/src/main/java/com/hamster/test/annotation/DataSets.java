a7714f95d7bc6ed505a39eb927ec631aa8c6dc99
hamster1980 <79819721701@yandex,ru>
2014-03-16 23:35:50 +0400

    type data set

--- test/src/main/java/com/hamster/test/annotation/DataSets.java
+++ test/src/main/java/com/hamster/test/annotation/DataSets.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 import org.apache.commons.lang3.StringUtils;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD ,ElementType.TYPE})
 public @interface DataSets {
 
 	String setUpDataSet() default StringUtils.EMPTY;
--- test/src/main/java/com/hamster/test/annotation/ServiceTestExecutionListener.java
+++ test/src/main/java/com/hamster/test/annotation/ServiceTestExecutionListener.java
@@ -2,15 +2,19 @@ package com.hamster.test.annotation;
 
 import org.apache.commons.lang3.StringUtils;
 import org.dbunit.IDatabaseTester;
+import org.dbunit.dataset.CompositeDataSet;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
 import org.dbunit.util.fileloader.XlsDataFileLoader;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 
 public class ServiceTestExecutionListener implements TestExecutionListener {
 
-	public static final String DATABASE_TESTER_BEAN = "databaseTester",
-								XLS_DATA_FILE_LOADER_BEAN = "xlsDataFileLoader";
+	public static final String DATABASE_TESTER_BEAN = "databaseTester";
+	public static final String XLS_DATA_FILE_LOADER_BEAN = "xlsDataFileLoader";
 	
+	private IDataSet typeDataSet;
 	private IDatabaseTester databaseTester;
 	
 	@Override
@@ -25,23 +29,43 @@ public class ServiceTestExecutionListener implements TestExecutionListener {
 	}
 
 	@Override
-	public void beforeTestClass(TestContext arg0) throws Exception {
+	public void beforeTestClass(TestContext testContext) throws Exception {
+		DataSets dataSets = testContext.getTestClass().getAnnotation(DataSets.class);
+		if(dataSets == null || StringUtils.isEmpty(dataSets.setUpDataSet())) {
+			return;
+		}
+		typeDataSet = load(testContext, dataSets);
 	}
 
 	@Override
 	public void beforeTestMethod(TestContext testContext) throws Exception {
-		DataSets dataSets = testContext.getTestMethod().getAnnotation(DataSets.class);
-		if(dataSets == null || StringUtils.isEmpty(dataSets.setUpDataSet())) {
+		IDataSet dataSet = compositeDataSet(testContext, typeDataSet, testContext.getTestMethod().getAnnotation(DataSets.class));
+		if(dataSet == null) {
 			return;
 		}
 		databaseTester = testContext.getApplicationContext().getBean(DATABASE_TESTER_BEAN, IDatabaseTester.class);
-		XlsDataFileLoader loader = testContext.getApplicationContext().getBean(XLS_DATA_FILE_LOADER_BEAN, XlsDataFileLoader.class);
-		databaseTester.setDataSet(loader.load(dataSets.setUpDataSet()));
+		databaseTester.setDataSet(dataSet);
 		databaseTester.onSetup();
 	}
 
+	private IDataSet compositeDataSet(TestContext testContext, IDataSet typeDataSet, DataSets methodDataSets) throws DataSetException {
+		if(methodDataSets == null || StringUtils.isEmpty(methodDataSets.setUpDataSet())) {
+			return typeDataSet;
+		}
+		IDataSet methodDataSet = load(testContext, methodDataSets); 
+		if(typeDataSet == null) {
+			return methodDataSet; 
+		}
+		return new CompositeDataSet(typeDataSet, methodDataSet);
+	}
+	
 	@Override
 	public void prepareTestInstance(TestContext arg0) throws Exception {
 	}
 
+	private IDataSet load(TestContext testContext, DataSets dataSets) {
+		XlsDataFileLoader loader = testContext.getApplicationContext().getBean(XLS_DATA_FILE_LOADER_BEAN, XlsDataFileLoader.class);
+		return loader.load(dataSets.setUpDataSet());
+		
+	}
 }
